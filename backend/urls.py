fe6c0d23d75c835cb2304655b0d7113d179f820b
Sergio Graca <sergiofgraca@gmail.com>
2026-04-27 13:42:46 -0300

    fix: urls separadas corretamente

--- backend/urls.py
+++ backend/urls.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-from rest_framework_simplejwt.views import TokenObtainPairView
-
-
-def home(request):
-    return HttpResponse("API ONLINE 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('usuarios.urls')),
-    path('', home),
-    path('api/token/', TokenObtainPairView.as_view()),
 ]
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,23 +1,12 @@
 from django.urls import path
-
-from .views import salvar_musica
-from .views import salvar_musica, listar_musicas
-from .views import deletar_musica
-from .views import ranking
-from django.contrib import admin
-from django.urls import path, include
-from .views import criar_admin
+from .views import salvar_musica, listar_musicas, deletar_musica, ranking, criar_admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # 👈 ESSENCIAL
-    path('api/', include('usuarios.urls')),
-    path('criar-admin/', criar_admin),
-]
-
-urlpatterns = [
-   
     path('salvar/', salvar_musica),
     path('listar/', listar_musicas),
     path('deletar/<int:id>/', deletar_musica),
     path('ranking/', ranking),
+
+    # 🔥 TEMPORÁRIO
+    path('criar-admin/', criar_admin),
 ]
