314d91ca96992e2dace961ce9dd39ba064496ae2
Sergio Graca <sergiofgraca@gmail.com>
2026-04-29 02:07:46 -0300

    fix: corrigido import path no urls.py (crash backend)

--- backend/urls.py
+++ backend/urls.py
@@ -1,7 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include  # ✅ AQUI ESTÁ O QUE FALTAVA
+
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/', include('usuarios.urls')),
+
+    # 🔥 LOGIN JWT
     path('api/token/', TokenObtainPairView.as_view()),
 ]
