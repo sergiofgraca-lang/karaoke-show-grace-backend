6592e3a705ca47b3bb8aa2e9ddb8e670b378da46
Sergio Graca <sergiofgraca@gmail.com>
2026-04-29 02:04:41 -0300

    fix: adiciona rota JWT /api/token

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('api/', include('usuarios.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
 ]
