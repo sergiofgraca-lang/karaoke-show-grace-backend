e20e9cbea5b3110541fb6f835ce9278a7fb0d3d4
Sergio Graca <sergiofgraca@gmail.com>
2026-09-16 12:51:29 -0300

    testa cliente android testsuite na vercel

--- usuarios/views.py
+++ usuarios/views.py
@@ -124,7 +124,7 @@ def teste_bgutil(request):
 
             "extractor_args": {
                 "youtube": {
-                   "player_client": ["android_creator"],
+                   "player_client": ["android_testsuite"],
                 },
                 "youtubepot-bgutilhttp": {
                     "base_url": "https://bgutil-ytdlp-pot-provider-0f67.onrender.com"
